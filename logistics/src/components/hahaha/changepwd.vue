b4e1cf57a688bc1b686c9462dc74f1a997fd1f57
baijingru <bjrbai@cn.ibm.com>
2018-02-24 18:53:55 +0900

    我将test by LiuWeiWei改成了test by Baijingru

---
+++ logistics/src/components/hahaha/changepwd.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <mt-header fixed title="更改密码">
+      <div slot="left">
+        <mt-button v-link="'/'" icon="back"></mt-button>
+        <span>我的</span>
+      </div>
+    </mt-header>
+
+    <div style="margin-top: 80pt"></div>
+
+    <form>
+      <span style="margin-left: 30px">原密码：</span>
+      <input class="inline" style="margin-top: 30pt;margin-left: 30px" placeholder="输入原始密码"><br>
+      <span style="margin-left: 30px">原密码：</span>
+      <input class="inline" style="margin-top: 30pt;margin-left: 30px" placeholder="输入原始密码"><br>
+      <span style="margin-left: 30px">原密码：</span>
+      <input class="inline" style="margin-top: 30pt;margin-left: 30px" placeholder="输入原始密码"><br>
+    </form>
+    <center><mt-button type="primary" style="width:200px; margin-top: 20px">提交</mt-button></center>
+
+  </div>
+
+
+
+
+
+
+
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+
+      }
+    },
+    components:{
+
+    },
+    mounted:function(){
+
+    },
+    computed:{
+
+    },
+    methods:{
+
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+
+  input{display:block; width:280px; height:40px; line-height:40px; margin:0 auto; margin-bottom: 10px;
+    outline:none; border:1px solid #888; padding:10px; box-sizing:border-box;}
+  span{cursor:pointer;}
+  .inline
+  {
+    display:inline;
+  }
+
+</style>
---
+++ logistics/src/components/hahaha/user.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <mt-header fixed title="大连市">
+      <div slot="left">
+        <mt-button v-link="'/'" icon="back">返回</mt-button>
+        <mt-button @click="handleClose">关闭</mt-button>
+      </div>
+      <i class="icon-mine" slot="right"></i>
+    </mt-header>
+
+    <div>
+      <center><img style="margin-top: 90pt" src="../../assets/jikuaidi.png" height="140" width="140"/></center>
+      <center><img style="margin-top: 40pt" src="../../assets/myorder.png" height="140" width="140"/></center>
+</div>
+
+    </div>
+
+
+
+
+</template>
+
+<script>
+    export default {
+        data () {
+          return {
+
+          }
+        },
+        components:{
+
+        },
+        mounted:function(){
+
+        },
+        computed:{
+
+        },
+        methods:{
+
+        }
+    }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+
+  .circle
+    background-color: #ffffff
+    font-size: $font-size-large
+
+
+
+</style>
