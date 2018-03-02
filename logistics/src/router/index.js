9b3786da0db64dd068d11ce518caf01836b6ab02
LiuWeiWei <wwliudl@cn.ibm.com>
2018-03-02 18:00:22 +0800

    2018/2/24  制作个人信息画面

--- logistics/src/router/index.js
+++ logistics/src/router/index.js
@@ -4,24 +4,37 @@ import logon from 'components/logon/logon'
 import mheader from 'components/m-header/m-header'
 import home from 'components/home/home'
 import App from '../App'
+import register from 'components/register/register'
+import car from 'components/car/car'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      redirect: 'home'
-    },
-    {
-      path: '/home',
-      component: home
-    },
+    // {
+    //   path: '/',
+    //   redirect: 'home'
+    // },
+    // {
+    //   path: '/home',
+    //   component: home
+    // },
     {
       path: '/logon',
       name:'logon',
       component: logon
-    }
-    ]
+    },
+    {
+      path: '/register',
+      name:'register',
+      component: register
+    },
+    // {
+    //   path: '/car',
+    //   name:'car',
+    //   component: car
+    // }
+    ],
+  mode:"history"
   })
 
 
