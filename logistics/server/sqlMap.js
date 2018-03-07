fdf0e18ea905921659c4e877b3c0116a8555ad47
LiuWeiWei <wwliudl@cn.ibm.com>
2018-03-07 17:53:12 +0800

    2018/03/07

--- logistics/server/sqlMap.js
+++ logistics/server/sqlMap.js
@@ -3,6 +3,9 @@ var sqlMap = {
   user: {
     add: 'insert into user(username ,password) values(?, ?)',
     select: 'select * from user where username=? and password=?'
+  },
+  order: {
+    select: 'select * from order where orderId=?'
   }
 }
 module.exports = sqlMap;
